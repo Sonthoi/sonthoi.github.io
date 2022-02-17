abf8984f7fd06a8d72ef42e0d5c5a1cb903ea55a
Sonthoi <99712848+Sonthoi@users.noreply.github.com>
2022-02-17 12:10:23 +0700

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -9,23 +9,13 @@ class Sonthoi {
       id: 'sonthoi',
       name: 'Sonthoi',
 
-      color1: '#8BC34A',
-      color2: '#7CB342',
-      color3: '#689F38',
+      color1: '#ADD8E6',
+      color2: '#ADD8E6',
+      color3: '#ADD8E6',
 
       menuIconURI: icon,
 
       blocks: [
-        {
-          opcode: 'trueBlock',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'true'
-        },
-        {
-          opcode: 'falseBlock',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'false'
-        },
         {
           opcode: 'reportValue',
           blockType: Scratch.BlockType.COMMAND,
@@ -33,49 +23,7 @@ class Sonthoi {
           text: 'report value [STRING]',
           arguments: {
             STRING: {
-              type: Scratch.ArgumentType.BOOLEAN
-            }
-          }
-        },
-        {
-          opcode: 'letters',
-
-          blockType: Scratch.BlockType.REPORTER,
-
-          text: 'letters [START] to [END] of [STRING]',
-          arguments: {
-            START: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: 5
-            },
-            END: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: 7
-            },
-            STRING: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'red apple'
-            }
-          }
-        },
-        {
-          opcode: 'regexReplace',
-
-          blockType: Scratch.BlockType.REPORTER,
-
-          text: 'replace [STRING] using the rule [REGEX] with [NEWSTRING]',
-          arguments: {
-            STRING: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'bananas are awesome. i like bananas.'
-            },
-            REGEX: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'banana'
-            },
-            NEWSTRING: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'apple'
+              type: Scratch.ArgumentType.STRING
             }
           }
         }
@@ -83,80 +31,9 @@ class Sonthoi {
     }
   }
 
-  isExactly({A, B}) {
-    return A === B;
-  }
-
-  isLessOrEqual({A, B}) {
-    return A <= B;
-  }
-
-  isMoreOrEqual({A, B}) {
-    return A >= B;
-  }
-
-  trueBlock() {
-    return true;
-  }
-
-  falseBlock() {
-    return false;
-  }
-
   reportValue({STRING}) {
     return STRING
   }
-
-  pi() {
-    return Math.PI;
-  }
-
-  ternaryOperator({A, B, C}) {
-    return A ? B : C;
-  }
-
-  letters({STRING, START, END}) {
-    return STRING.slice(Math.max(1, START) - 1, Math.min(STRING.length, END));
-  }
-
-  currentMillisecond() {
-    return Date.now() % 1000;
-  }
-
-  fetchFrom({URL}) {
-    return fetch(URL).then(res => res.text())
-      .catch(err => '');
-  }
-
-  parseJSON({PATH, JSON_STRING}) {
-    try {
-      const path = PATH.toString().split('/').map(prop => decodeURIComponent(prop));
-      if (path[0] === '') path.splice(0, 1);
-      if (path[path.length - 1] === '') path.splice(-1, 1);
-      let json;
-      try {
-        json = JSON.parse(' ' + JSON_STRING);
-      } catch (e) {
-        return e.message;
-      }
-      path.forEach(prop => json = json[prop]);
-      if (json === null) return 'null';
-      else if (json === undefined) return '';
-      else if (typeof json === 'object') return JSON.stringify(json);
-      else return json.toString();
-    } catch (err) {
-      return '';
-    }
-  }
-
-  stringToBoolean({STRING}) {
-    return STRING;
-  }
-
-  regexReplace({STRING, REGEX, NEWSTRING}) {
-    return STRING.toString().replace(new RegExp(REGEX, 'gi'), NEWSTRING);
-  }
-
 }
 
 Scratch.extensions.register(new Sonthoi());
